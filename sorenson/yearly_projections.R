3bb3fc7fdeb0f5a4dd50019c00d0afb9ef240473
Jon Zadra <jon.zadra@sorensonimpact.com>
2019-09-27 17:16:41 -0600

    added geo identifiers

--- sorenson/yearly_projections.R
+++ sorenson/yearly_projections.R
@@ -2,24 +2,12 @@ require(tidyverse)
 require(siverse)
 require(furrr)
 
-ssp <- read_csv("~/Google Drive/SI/DataScience/data/gates/Hauer Population Projections/SSP_asrc.csv")
-
-ssp <- ssp %>% clean_names()
-
-
-ssp_interp <- ssp %>% 
-  group_by(geoid, race, state, county, year) %>% 
-  summarize(total_pop = sum(ssp2)) %>% 
-  nest() %>% 
-  mutate(total_pop_interp = map(data, ~approx(x = .$year, y = .$total_pop, xout = 2020:2100) %>% as_tibble())) %>% 
-  unnest(total_pop_interp) %>% 
-  rename(year = x, total_pop_interp = y) %>% 
-  select(-data) %>% 
-  mutate(race = recode(race, `1` = "White NH", `2` = "Black NH", `3` = "Hispanic", `4` = "Other NH")) %>% 
-  left_join(fips_codes %>% select(state_abb = state, state = state_code, state_name))
-
-
 source('./SCRIPTS/003-proj_basedataload.R')
+
+ssp <- read_csv("~/Google Drive/SI/DataScience/data/gates/Hauer Population Projections/SSP_asrc.csv") %>% 
+  clean_names()
+
+plan(multisession)
 
 past <- K05_pop %>% clean_names() %>% 
   ungroup() %>% 
@@ -41,28 +29,26 @@ poprocks <- bind_rows(past, fut) %>%
   select(-county_rows) %>% 
   group_by(geoid, race, state, county, county_changed) %>% 
   nest() %>% 
-  mutate(total_pop_interp = map(data, ~approx(x = .$year, y = .$total_pop, xout = 1990:2100) %>% as_tibble())) %>% 
+  mutate(total_pop_interp = future_map(data, ~approx(x = .$year, y = .$total_pop, xout = 1990:2100) %>% as_tibble())) %>% 
   unnest(total_pop_interp) %>% 
   rename(year = x, total_pop_interp = y) %>% 
   select(-data) %>% 
   mutate(race = recode(race, `1` = "White NH", `2` = "Black NH", `3` = "Hispanic", `4` = "Other NH")) %>% 
-  left_join(fips_codes %>% select(state_abb = state, state = state_code, state_name) %>% distinct())
+  left_join(fips_codes %>% select(state_abb = state, state = state_code, state_name) %>% distinct()) %>%
+  left_join(fips_codes %>% mutate(geoid = paste0(state_code, county_code)) %>% select(geoid, county_name = county) %>% distinct(), by = "geoid")
 
+#Save
+poprocks %>% filter(between(year, 1990, 2030)) %>% write_rds("~/Google Drive/SI/DataScience/data/gates/Hauer Population Projections/county_pop_race_1990_2030.rds")
 
-
-
-
-
-
-
-
-
-
+#Look at some of this
 ssp_state <- poprocks %>% 
   group_by(state_name, year, race) %>% 
-  summarize(state_race_pop = sum(total_pop_interp))
+  summarize(state_race_pop = sum(total_pop_interp, na.rm = T))
 
 ssp_state %>% 
+  filter(between(year, 2017, 2020)) %>% 
   ggplot(aes(x = year, y = state_race_pop, color = race, fill = race)) +
-  geom_point() + geom_line() + facet_wrap(~state_name, scales = "free_y")
+  geom_line() + facet_wrap(~state_name, scales = "free_y")
 
+ssp_state %>% 
+  filter(state_name == "Alaska", between(year, 2017, 2020))
