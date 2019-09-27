eeec377f32fd114ff58d7d3cd3d211596d902476
Jon Zadra <jon.zadra@sorensonimpact.com>
2019-09-27 14:27:49 -0600

    initial commit

---
+++ sorenson/yearly_projections.R
@@ -0,0 +1,68 @@
+require(tidyverse)
+require(siverse)
+require(furrr)
+
+ssp <- read_csv("~/Google Drive/SI/DataScience/data/gates/Hauer Population Projections/SSP_asrc.csv")
+
+ssp <- ssp %>% clean_names()
+
+
+ssp_interp <- ssp %>% 
+  group_by(geoid, race, state, county, year) %>% 
+  summarize(total_pop = sum(ssp2)) %>% 
+  nest() %>% 
+  mutate(total_pop_interp = map(data, ~approx(x = .$year, y = .$total_pop, xout = 2020:2100) %>% as_tibble())) %>% 
+  unnest(total_pop_interp) %>% 
+  rename(year = x, total_pop_interp = y) %>% 
+  select(-data) %>% 
+  mutate(race = recode(race, `1` = "White NH", `2` = "Black NH", `3` = "Hispanic", `4` = "Other NH")) %>% 
+  left_join(fips_codes %>% select(state_abb = state, state = state_code, state_name))
+
+
+source('./SCRIPTS/003-proj_basedataload.R')
+
+past <- K05_pop %>% clean_names() %>% 
+  ungroup() %>% 
+  select(-countyrace) %>% 
+  group_by(geoid, race, state, county, year) %>% 
+  summarize(total_pop = sum(population)) %>% 
+  ungroup() %>% 
+  mutate(race = as.numeric(race))
+
+fut <- ssp %>% 
+  group_by(geoid, race, state, county, year) %>% 
+  summarize(total_pop = sum(ssp2))
+
+poprocks <- bind_rows(past, fut) %>% 
+  arrange(geoid, race, state, county, year) %>% 
+  filter(geoid != "99999") %>% 
+  add_count(geoid, name = "county_rows") %>% 
+  mutate(county_changed = county_rows < 180) %>% 
+  select(-county_rows) %>% 
+  group_by(geoid, race, state, county, county_changed) %>% 
+  nest() %>% 
+  mutate(total_pop_interp = map(data, ~approx(x = .$year, y = .$total_pop, xout = 1990:2100) %>% as_tibble())) %>% 
+  unnest(total_pop_interp) %>% 
+  rename(year = x, total_pop_interp = y) %>% 
+  select(-data) %>% 
+  mutate(race = recode(race, `1` = "White NH", `2` = "Black NH", `3` = "Hispanic", `4` = "Other NH")) %>% 
+  left_join(fips_codes %>% select(state_abb = state, state = state_code, state_name) %>% distinct())
+
+
+
+
+
+
+
+
+
+
+
+ssp_state <- poprocks %>% 
+  group_by(state_name, year, race) %>% 
+  summarize(state_race_pop = sum(total_pop_interp))
+
+ssp_state %>% 
+  ggplot(aes(x = year, y = state_race_pop, color = race, fill = race)) +
+  geom_point() + geom_line() + facet_wrap(~state_name, scales = "free_y")
+
